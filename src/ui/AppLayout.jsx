0ee993d833bb65aa24cc3286d312ed4421d3da30
okharyton <olegkhacv@gmail.com>
2024-07-03 19:00:27 +0300

    feat: Building the App Layout p.2

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,15 +1,30 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
 import Sidebar from "./Sidebar.jsx";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding:
+    4rem 4,
+    8rem 6.4rem;
+`;
 
 export default function AppLayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
-      <main>
+      <Main>
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 }
