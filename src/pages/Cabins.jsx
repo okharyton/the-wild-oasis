73b6d335272c1ab8fb8cbbbd4d867a7a0a2b1a92
okharyton <olegkhacv@gmail.com>
2024-07-10 19:18:43 +0300

    feat: connect supabase to project

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,20 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { useEffect } from "react";
+import { getCabins } from "../services/apiCabins.js";
 
 function Cabins() {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://lbcxdkiprbcmahvxppse.supabase.co/storage/v1/object/public/cabin-images/cabin_001.jpg"
+        alt="cabin-001"
+      />
     </Row>
   );
 }
