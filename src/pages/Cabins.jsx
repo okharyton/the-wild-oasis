f41f5b63b20469e34364a1c75e8124fa08afca02
okharyton <olegkhacv@gmail.com>
2024-07-11 16:52:37 +0300

    feat: add fetching cabins data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,20 +2,19 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { useEffect } from "react";
 import { getCabins } from "../services/apiCabins.js";
+import CabinTable from "../features/cabins/CabinTable.jsx";
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then((data) => console.log(data));
-  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://lbcxdkiprbcmahvxppse.supabase.co/storage/v1/object/public/cabin-images/cabin_001.jpg"
-        alt="cabin-001"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
