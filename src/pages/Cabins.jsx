640fd33072f9d7b39a6fe1876ebf1b3b02794648
okharyton <olegkhacv@gmail.com>
2024-07-20 22:26:02 +0300

    feat: add react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getCabins } from "../services/apiCabins.js";
 import CabinTable from "../features/cabins/CabinTable.jsx";
+import Button from "../ui/Button.jsx";
+import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -13,6 +16,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((form) => !form)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
